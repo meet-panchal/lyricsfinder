710b6a155216567c6c316748a8f4caf6c98094b6
Meet Panchal <MEET.PANCHAL@pb.com>
2019-05-01 13:28:56 +0530

    Lyrics Finder Using React.js

---
+++ src/context.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+const Context = React.createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH_TRACKS':
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: 'Search Results'
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+    state = {
+        track_list :[],
+        heading: "Top 10 Results",
+        dispatch: action => this.setState(state => reducer(state, action))
+    }
+    componentDidMount(){
+        axios.get('https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=in&f_has_lyrics=1&apikey=a30c2eed6dfac8f1175d1572dc9a78c0')
+        .then(res => {
+            //console.log(res.data)
+            this.setState({
+                track_list : res.data.message.body.track_list
+            })
+        })
+        .catch(err => console.log(err));
+    }
+  render() {
+    return (
+      <Context.Provider value = {this.state}>
+          {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+
+export const Consumer = Context.Consumer;
